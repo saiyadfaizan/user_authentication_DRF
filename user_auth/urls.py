c5d7d3713c774f1c4b7740ae7a9a39205c61134f
saiyadfaizan <saliali@bestpeers.com>
2020-12-03 16:56:22 +0530

    Initial commit

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -19,7 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_auth_app.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
 
